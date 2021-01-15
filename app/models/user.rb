cf9413ad73d9509cd2605f741708dff4ec14f0ab
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-15 12:30:08 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/.freeze
+ NAME_KANA_REGEX = /\A[ァ-ヶ]+\z/.freeze
   validates :nickname, presence: true
   validates_format_of :password, with:PASSWORD_REGEX
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence:true, format:{with:NAME_REGEX} do
+    validates :family_name, presence: true
+    validates :first_name, presence: true
+  end
+  with_options presence:true, format:{with:NAME_KANA_REGEX} do
+    validates :family_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
   validates :birthday, presence: true
 end
