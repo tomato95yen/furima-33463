0539efa3cc9a76f3e86ccd2d86e10d68cc1b92e1
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-16 20:52:42 +0900

    Active Hashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,20 +4,26 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-end
 
-private
-def move_to_sign_in
-  unless user_signed_in?
-    redirect_to new_user_session_path
+  private
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
-end
 
-def item_params
+  def item_params
   params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, :item_price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_day
 
   with_options presence: true do
     validates :name
@@ -13,5 +19,12 @@ class Item < ApplicationRecord
     validates :user_id
   end
   validates :item_price, presence: true, numericality:{only_integer: true, geater_than: 300, less_than: 9999999}, format: {with: /\A[0-9]+\z/}
-  
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+  end
 end
