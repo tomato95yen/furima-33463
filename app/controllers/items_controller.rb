19b31217fb212ed8e827b782f335c6ad979fb514
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-20 18:58:42 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :user_validate, only: :edit
-  before_action :find_item, only: [:edit, :show]
+  before_action :find_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -27,11 +27,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
-      @item = item
       render :edit
     end
   end
