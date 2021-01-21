24a14c0b624877db80693bac0a2a52c08a597602
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-21 11:58:51 +0900

    ordersコントローラーにcreateアクション追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,20 @@
 class OrdersController < ApplicationController
   def index
-    
+    @item = Item.find(params[:item_id])
+  end
+
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(item_order: current_user.id)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,13 @@ class Order < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belings_to :shipping_area
 
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :block
+    validates :phone_number
+  end
+
   validates :prefecture_id, numericality: { other_than: 1 }
 end
