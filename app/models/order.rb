897a16b6e4d65b8271e8988ca8585bb7f92080ad
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-21 09:45:21 +0900

    都道府県選択のタブを実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belings_to :shipping_area
+
+  validates :prefecture_id, numericality: { other_than: 1 }
 end
