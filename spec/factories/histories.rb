46a02a4b3962b2020175613f0c6f7d3eb810eac9
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-26 13:36:31 +0900

    historyモデルの単体テストコード準備

--- spec/factories/histories.rb
+++ spec/factories/histories.rb
@@ -4,11 +4,8 @@ FactoryBot.define do
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city { "横浜市緑区" }
     block { "青山1-1-1" }
-    building { "柳ビル103" }
+    building_name { "柳ビル103" }
     phone_number { Faker::PhoneNumber.subscriber_number(length: 11) }
     token { "tok_abcdefghijk00000000000000000" }
-    user_id { "" }
-    item_id { "" }
-
   end
 end
--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe History, type: :model do
-  
+  describe '購入情報の保存' do
+    before do
+      @history = FactoryBot.build(:history)
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@history).to be_valid
+    end
+  end
 end
