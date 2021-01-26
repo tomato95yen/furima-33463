d3adde6fbd7d223f0a2af8347d88bec933d2e8b7
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-26 11:53:40 +0900

    売却済み商品の購入画面へアクセスできないように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :item_sold
   def index
     @item = Item.find(params[:item_id])
     @history = History.new
@@ -25,5 +27,14 @@ class OrdersController < ApplicationController
   private
   def history_params
     params.require(:history).permit(:postal_code,:prefecture_id, :city, :block, :phone_number, :user_item_id, :user_id).merge(token: params[:token], user_id: current_user.id, item_id: @item.id )
+  end
+
+  def item_sold
+    user_items = UserItem.all
+    item_ids = user_items.pluck(:item_id)
+    @item = Item.find(params[:item_id])
+    if item_ids.include?(@item.id)
+      redirect_to root_path
+    end
   end
 end
