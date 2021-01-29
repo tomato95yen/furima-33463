e37b3a85e28eceba26320c69b3613a62cfa0696c
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-29 16:16:45 +0900

    画像のプレビュー機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../card")
-require("../item_price");
+require("../item_price")
+require("../preview");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
