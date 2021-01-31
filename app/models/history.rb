7814768fe3e8690f71c2fb95a77a11727dc724d2
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-31 19:02:40 +0900

    historyモデルの修正

--- app/models/history.rb
+++ app/models/history.rb
@@ -4,7 +4,7 @@ class History
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{,11}\z/ }
